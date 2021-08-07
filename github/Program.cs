6c8d207be9de8d2a7786afb19f8ad08820b87572
Doroteo Corona Garcia <dcg_jaguar@hotmail.com>
2021-08-07 17:37:36 -0500

    Agregar archivos de proyecto.

---
+++ github/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.TeamFoundation.WorkItemTracking.WebApi;
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models;
+using Microsoft.VisualStudio.Services.Common;
+using Microsoft.VisualStudio.Services.WebApi;
+using System;
+using System.Threading.Tasks;
+
+namespace github
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri orgUrl = new Uri("https://github.com/YesiMoBa/Safer-Management-System.git"); // Organization URL
+            String personalAccessToken = "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";//personalAccessToken
+            int workItemId = 2; // ID of a work item, for example:12
+
+            // Create a connection 
+            VssConnection connection = new VssConnection(orgUrl, new VssBasicCredential(string.Empty, personalAccessToken));
+
+            //Show details a work item
+            ShowWorkItemDetails(connection, workItemId).Wait();
+
+        }
+        static private async Task ShowWorkItemDetails(VssConnection connection, int workItemId)
+        {
+            // Get an instance of the work item tracking client
+            WorkItemTrackingHttpClient witClient = connection.GetClient<WorkItemTrackingHttpClient>();
+
+            try
+            {
+                // Get the specified wor item
+                WorkItem workitem = await witClient.GetWorkItemAsync(workItemId);
+
+                // Output the work item´s field values
+                foreach (var field in workitem.Fields)
+                {
+                    Console.WriteLine(" {0}: {1}", field.Key, field.Value);
+                }
+            }
+            catch (AggregateException aex)
+            {
+                VssServiceException vssex = aex.InnerException as VssServiceException;
+                if (vssex != null)
+                {
+                    Console.WriteLine(vssex.Message);
+                }
+            }
+        }
+    }
+}
